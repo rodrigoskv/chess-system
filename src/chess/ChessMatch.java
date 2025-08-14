756161dd2e9b35f28afd095b80c79d2c729a140d
rodrigoskv <rodrigotelesskv@gmail.com>
2025-08-14 18:19:58 -0300

    Add ANSI color output and update chess setup

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,11 +32,19 @@ public class ChessMatch {
 	}
 	
 	private void initialSetuo() {
-		placeNewPiece('a',8,new Rook(board, Color.WHITE));
-		placeNewPiece('b',8,new Knight(board, Color.WHITE));
-		placeNewPiece('c',8,new Bishop(board, Color.WHITE));
-		placeNewPiece('d',8,new Queen(board, Color.WHITE));
-		placeNewPiece('e',8,new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 	}
 	
