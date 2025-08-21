ee1ebd2645cf635335d30badc2cbc04e0756ee45
rodrigoskv <rodrigotelesskv@gmail.com>
2025-08-21 16:02:10 -0300

    implementando movimentos possíveis da Torre (Rook)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece)capturedPiece;
 	}
@@ -40,6 +41,13 @@ public class ChessMatch {
 		if(!board.piece(position).isThereAnyPossibleMoves()) {
 			throw new ChessException("Não existe movimentos possíveis para a peça escolhida");
 		}
+	}
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if(!board.piece(source).possibleMove(target)) {
+			throw new ChessException("A peça escolhida não pode se mover para a posição de destino");
+		}
+		
 	}
 	
 	private Piece makeMove(Position source, Position target) {
