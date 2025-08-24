b95aa57b9f10db49d66793b20997a368d927c5e4
Rodrigo Miguel Teles Dos Santos <166267162+rodrigoskv@users.noreply.github.com>
2025-08-24 11:00:18 -0300

    Armazenando as peças capturadas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -13,6 +16,9 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 
+	private List<ChessPiece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
+	
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
@@ -83,12 +89,17 @@ public class ChessMatch {
 		Piece captured = board.removePiece(target);
 		board.placePiece(p, target);
 
+		if(captured != null) {
+			piecesOnTheBoard.remove(captured);
+			capturedPieces.add(captured);
+		}
 		return captured;
 
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 
 	private void initialSetuo() {
